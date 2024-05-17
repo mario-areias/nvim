487d507e76410c8ba29988f26b8560c63fbbe285
Mario Areias <mario.s.s.areias@gmail.com>
2024-05-18 01:36:37 +1000

    lualine shows recording messages

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,13 @@
+local function show_macro_recording()
+	local recording_register = vim.fn.reg_recording()
+
+	if recording_register == "" then
+		return ""
+	else
+		return "Recording @" .. recording_register
+	end
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -5,6 +15,20 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "catppuccin-mocha",
+				refresh = {
+					statusline = 200, -- Note these are in mili second and default is 1000
+				},
+			},
+			-- add sectin for macro recording:
+			-- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#show-recording-messages
+			sections = {
+				lualine_x = {
+					{ "show_macro_recording", fmt = show_macro_recording },
+					-- these 3 are the default sections
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
 			},
 		})
 	end,
