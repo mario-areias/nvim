1e91fd83b289c1ec528cbc1f332de3dcfcf4a579
Mario Areias <mario.s.s.areias@gmail.com>
2024-05-26 01:27:12 +1000

    installing rust dap and mason-nvim-dap

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,50 +1,90 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"leoluz/nvim-dap-go",
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"theHamsta/nvim-dap-virtual-text",
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+			"theHamsta/nvim-dap-virtual-text",
+		},
+		config = function()
+			require("dapui").setup()
+			require("dap-go").setup()
+			require("nvim-dap-virtual-text").setup()
+
+			local dap, dapui, dapgo = require("dap"), require("dapui"), require("dap-go")
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			-- Unfortunatly cappuccin doesn't have a pre-configured setting for this using a background
+			-- Since I prefer high contrast, I am rolling out this custom config
+			vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+
+			vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "", linehl = "DapBreakpoint", numhl = "" })
+			vim.fn.sign_define(
+				"DapStopped",
+				{ text = "→", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+			)
+
+			vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "[D]AP: Toggle [B]reakpoint" })
+			vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "[D]AP: Run/[C]ontinue" })
+			vim.keymap.set("n", "<Leader>dx", dap.disconnect, { desc = "[D]AP: Terminate" })
+			vim.keymap.set("n", "<Leader>do", dap.step_over, { desc = "[D]AP: Step [o]ver" })
+			vim.keymap.set("n", "<Leader>dO", dap.step_out, { desc = "[D]AP: Step [O]ut" })
+			vim.keymap.set("n", "<Leader>di", dap.step_into, { desc = "[D]AP: Step [I]nto" })
+
+			-- This mapping only works for Go. It is a custom mapping to run the test close to the cursor
+			-- All other operations can be done by the above mappings
+			vim.keymap.set("n", "<Leader>gd", dapgo.debug_test, { desc = "[G]o: [D]ebug Test" })
+		end,
 	},
-	config = function()
-		require("dapui").setup()
-		require("dap-go").setup()
-		require("nvim-dap-virtual-text").setup()
 
-		local dap, dapui, dapgo = require("dap"), require("dapui"), require("dap-go")
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		opts = {
+			ensure_installed = { "codelldb", "delve" },
+			handlers = {
+				function(config)
+					-- all sources with no handler get passed here
+					-- Keep original functionality
+					require("mason-nvim-dap").default_setup(config)
+				end,
+				codelldb = function(config)
+					config.configurations = {
+						{
+							name = "LLDB: Launch(codellde.log ENABLED)",
+							type = "codelldb",
+							request = "launch",
+							program = function()
+								-- it assumes that the target is in the target/debug directory
+								local dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+								local exec = vim.fn.getcwd() .. "/target/debug/" .. dir
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
-
-		-- Unfortunatly cappuccin doesn't have a pre-configured setting for this using a background
-		-- Since I prefer high contrast, I am rolling out this custom config
-		vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
-
-		vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "", linehl = "DapBreakpoint", numhl = "" })
-		vim.fn.sign_define(
-			"DapStopped",
-			{ text = "→", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
-		)
-
-		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "[D]AP: Toggle [B]reakpoint" })
-		vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "[D]AP: Run/[C]ontinue" })
-		vim.keymap.set("n", "<Leader>dx", dap.disconnect, { desc = "[D]AP: Terminate" })
-		vim.keymap.set("n", "<Leader>do", dap.step_over, { desc = "[D]AP: Step [o]ver" })
-		vim.keymap.set("n", "<Leader>dO", dap.step_out, { desc = "[D]AP: Step [O]ut" })
-		vim.keymap.set("n", "<Leader>di", dap.step_into, { desc = "[D]AP: Step [I]nto" })
-
-		-- This mapping only works for Go. It is a custom mapping to run the test close to the cursor
-		-- All other operations can be done by the above mappings
-		vim.keymap.set("n", "<Leader>gd", dapgo.debug_test, { desc = "[G]o: [D]ebug Test" })
-	end,
+								vim.notify("Debugging: " .. exec)
+								return exec
+							end,
+							cwd = "${workspaceFolder}",
+							stopOnEntry = false,
+							args = {
+								-- https://github.com/vadimcn/codelldb/wiki/LLDB-Logging
+								-- initCommands = { "log enable gdb-remote default", "log enable lldb default" },
+							},
+						},
+					}
+					require("mason-nvim-dap").default_setup(config) -- don't forget this!
+				end,
+			},
+		},
+	},
 }
